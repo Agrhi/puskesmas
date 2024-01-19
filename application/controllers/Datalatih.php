92b727b3287afacf5c5038427e8e933fd7e0b382
Noval <muhammadnoval1998@gmail.com>
2024-01-19 10:15:20 +0800

    add fitur import data and delete all data in datalatih

--- application/controllers/Datalatih.php
+++ application/controllers/Datalatih.php
@@ -1,6 +1,11 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
+require 'vendor1/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Reader\Csv;
+
 class Datalatih extends CI_Controller
 {
 
@@ -23,5 +28,51 @@ class Datalatih extends CI_Controller
 		$this->load->view('layout/navbar');
 		$this->load->view('content/admin/datalatih/index', $data);
 		$this->load->view('layout/footer');
+	}
+
+	public function import()
+	{
+		$upload_file = $_FILES['upload-data-latih']['name'];
+		$extension = pathinfo($upload_file, PATHINFO_EXTENSION);
+		if ($extension == 'csv') {
+			$reader = new \PhpOffice\PhpSpreadsheet\Reader\Csv();
+		}
+		$spreadsheet = $reader->load($_FILES['upload-data-latih']['tmp_name']);
+		$sheetdata = $spreadsheet->getActiveSheet()->toArray();
+		$sheetcount = count($sheetdata);
+		if ($sheetcount > 1) {
+			$data = [];
+			for ($i = 1; $i < $sheetcount; $i++) {
+				$data_alamat = $sheetdata[$i][0];
+				$data_jk = $sheetdata[$i][1];
+				$data_umur = $sheetdata[$i][2];
+				$data_class = $sheetdata[$i][3];
+				$data[] = [
+					'alamat' => $data_alamat,
+					'jk' => $data_jk,
+					'umur' => $data_umur,
+					'class' => $data_class
+				];
+			}
+			$result = $this->Models_datalatih->insert_batch($data);
+			if ($result) {
+				$this->session->set_flashdata('swetalert', '`Good JoOb!`, `Data Berhasil di import`, `success`');
+			} else {
+				$this->session->set_flashdata('swetalert', '`Upsss!`, `Sorry., Data gagal di import`, `error`');
+			}
+			redirect('Datalatih');
+		}
+	}
+
+	public function deleteAll()
+	{
+		$result =
+			$this->Models_datalatih->truncateTable();
+		if ($result) {
+			$this->session->set_flashdata('swetalert', '`Good JoOb!`, `Data Berhasil di hapus`, `success`');
+		} else {
+			$this->session->set_flashdata('swetalert', '`Upsss!`, `Sorry., Data gagal di hapus`, `error`');
+		}
+		redirect('Datalatih');
 	}
 }
--- application/views/content/admin/dataLatih/index.php
+++ application/views/content/admin/dataLatih/index.php
@@ -2,7 +2,23 @@
     <div class="card">
         <div class="card-body">
             <div class="table-responsive">
-                <table class="table" id="syarat">
+                <table class="table caption-top" id="datalatih">
+                    <caption>
+                        <div class="row">
+                            <div class="text-end">
+                                <div class="btn-group" role="group">
+                                    <button type="button" class="btn btn-sm btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <i class="fa fa-download" aria-hidden="true"></i>
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="<?= base_url('/assets/format_data.csv'); ?>">Format Excel</a></li>
+                                        <li><a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#importdatalatih">Import Data</a></li>
+                                    </ul>
+                                </div>
+                                <a href="<?= base_url('Datalatih/deleteAll') ?>" class="btn bg-danger text-white btn-sm btn-act-trash" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete All Data"><i class="fa fa-trash" aria-hidden="true"></i></a>
+                            </div>
+                        </div>
+                    </caption>
                     <thead>
                         <tr>
                             <td>No.</td>
@@ -30,10 +46,34 @@
         </div>
     </div>
 
+    <!-- modal -->
+    <div class="modal fade" id="importdatalatih" tabindex="-1" role="dialog" aria-labelledby="importdatalatih" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="importdatalatih">Update Data</h5>
+                </div>
+                <form action="<?= base_url('Datalatih/import') ?>" method="POST" enctype="multipart/form-data">
+                    <div class="modal-body">
+                        <div class="col-sm-12">
+                            <div class="input-group">
+                                <input type="file" class="form-control" name="upload-data-latih" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload" required>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn bg-gradient-primary">Update</button>
+                        <button type="button" class="btn bg-gradient-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
 </section>
 
 <script>
     $(document).ready(function() {
-        $('#syarat').DataTable();
+        $('#datalatih').DataTable();
     });
 </script>
